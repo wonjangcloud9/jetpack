a0a81df3f6fc5884a18cc48d88960cc569e36897
WonJang <wj@WonJangui-noteubug.local>
2023-02-04 13:01:57 +0900

    result

--- app/src/main/java/com/example/compostutorial/MainActivity.kt
+++ app/src/main/java/com/example/compostutorial/MainActivity.kt
@@ -6,20 +6,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.Image
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            ResultScreen(bmi = 35.1)
         }
     }
 }
@@ -62,4 +68,34 @@ fun HomeScreen() {
             }
         }
     }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun ResultScreen(bmi: Double){
+     Scaffold(
+         topBar = {
+             TopAppBar(
+                 title = { Text("비만도 계산기")}
+             )
+
+         }
+     ) {
+         Column(
+             modifier = Modifier.fillMaxWidth(),
+             verticalArrangement = Arrangement.Center,
+             horizontalAlignment = Alignment.CenterHorizontally
+         ) {
+             Text("과체중", fontSize = 30.sp)
+             Spacer(modifier = Modifier.height(50.dp))
+             Image(
+                 painter = painterResource(id = R.drawable.baseline_sentiment_dissatisfied_24),
+                 contentDescription = null,
+                 modifier = Modifier.size(100.dp),
+                 colorFilter = ColorFilter.tint(
+                     color = Color.Black
+                 )
+             )
+         }
+     }
 }
